cce5a9372584979159fd8b631f763006d22b2eed
Olivier Martin <olivier.martin@b12-consulting.com>
2019-01-22 12:54:10 +0100

    Add mongoose and ts models/schema for Intervention

--- lib/models/contact.model.ts
+++ lib/models/contact.model.ts
@@ -33,3 +33,51 @@ export interface Contact {
   company?: string;
   phone?: Number;
 }
+
+export const InterventionSchema = new Schema({
+  seqno: { type: String },
+  streetname: { type: String },
+  houseno: { type: String },
+  cityname: { type: String },
+  longitude: { type: String },
+  latitude: { type: String },
+  numcasualties: { type: String },
+  description: { type: String },
+  remarks: { type: String },
+  callref: { type: String },
+  date: { type: String },
+  time: { type: String },
+  phoneno: { type: String },
+  callername: { type: String },
+  controlroomname: { type: String },
+  sectorcode: { type: String },
+  tripservicetype: { type: String },
+  tripservicename: { type: String }
+});
+
+export enum Description1733 {
+  visite_domicile = "1733 VISITE DOMICILE",
+  report = "1733 REPORT",
+  consultation = "1733 CONSULTATION PMG"
+}
+
+export interface Intervention {
+  seqno?: string;
+  streetname?: string;
+  houseno?: string;
+  cityname?: string;
+  longitude?: string;
+  latitude?: string;
+  numcasualties?: string;
+  description?: Description1733;
+  remarks?: string;
+  callref?: string;
+  date?: string;
+  time?: string;
+  phoneno?: string;
+  callername?: string;
+  controlroomname?: string;
+  sectorcode?: string;
+  tripservicetype?: string;
+  tripservicename?: string;
+}
